005df473826d8d3f78a681a56426b61ff50bb73a
unionthugface <cowofevil@aol.com>
2014-03-08 13:40:02 -0800

    trying again

---
+++ EnergyHack/Controllers/HomeApiController.cs
@@ -0,0 +1,41 @@
+﻿using EnergyHack.Models.Requests;
+using EnergyHack.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EnergyHack.Controllers
+{
+    [RoutePrefix("api/home")]
+    public class HomeApiController : ApiController
+    {
+        [Route("userinputsubmit")]
+        [HttpPost]
+        public HttpResponseMessage UserInputSubmit(UserInputRequest model)
+        {
+            UserInputResponse respModel = new UserInputResponse();
+
+            return Request.CreateResponse(HttpStatusCode.OK, respModel);
+
+        }
+
+        [Route("appliancesignal")]
+        [HttpGet]
+        public HttpResponseMessage ApplianceSignal(SignatureRequestModel model)
+        {
+            SignatureResponse respModel = new SignatureResponse();
+
+
+
+
+            return Request.CreateResponse(HttpStatusCode.OK, respModel);
+
+        }
+
+
+        
+    }
+}
