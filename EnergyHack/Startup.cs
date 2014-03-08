986e002835c76ea95a1dab24b6562e3588d3c825
unionthugface <cowofevil@aol.com>
2014-03-08 13:48:19 -0800

    rest of stuff

---
+++ EnergyHack/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnergyHack.Startup))]
+namespace EnergyHack
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
